ca032c5ebc592bc54a01ccfedfb3afbb062d50b9
Fabian Grochla <grochla95@outlook.com>
2019-09-15 22:00:01 +0200

    Improved ProductDao class

--- Product.MsSqlPersistance/ProductDao.cs
+++ Product.MsSqlPersistance/ProductDao.cs
@@ -11,56 +11,60 @@ namespace Product.MsSqlPersistance
 {
     public class ProductDao : IProductDao
     {
-        private readonly IDbConnection _db;
-
-        public ProductDao()
-        {
-            _db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);            
-        }
-
         public List<ProductSummary> GetProducts()
         {
-            return _db.Query<ProductSummary>("SELECT [Id],[Name],[Price] FROM [PRODUCTS]").ToList();
+            using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                return db.Query<ProductSummary>("SELECT [Id],[Name],[Price] FROM [PRODUCTS]").ToList();
+            }
         }
 
         public ProductSummary GetProduct(Guid id)
         {
-            return _db.Query<ProductSummary>("SELECT[id],[Name],[Price] FROM [PRODUCTS] WHERE Id =@Id",
-                new { Id = id }).SingleOrDefault();
+            using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                return db.QuerySingleOrDefault<ProductSummary>("SELECT[id],[Name],[Price] FROM [PRODUCTS] WHERE Id =@Id",
+                new { Id = id });
+            }
         }
 
         public Guid AddProduct(ProductSummary productSummary)
         {
-            return _db.Query<Guid>(
+            using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                return db.Query<Guid>(
                 @"DECLARE @InsertedRows AS TABLE (Id uniqueidentifier);
                     INSERT INTO Products ([Name],[Price]) OUTPUT Inserted.Id INTO @InsertedRows
                     VALUES (@name, @price);
                     SELECT Id FROM @InsertedRows",
                 new
                 {
-                    name = productSummary.Name, 
+                    name = productSummary.Name,
                     price = productSummary.Price
                 }).Single();
+            }
         }
 
         public void UpdateProduct(ProductSummary productSummary)
         {
-            var query = "UPDATE PRODUCTS SET Price = @price, " +
-                "Name = '" + productSummary.Name + "'" +
-                "WHERE Id = '" + productSummary.Id + "' OUTPUT";
-            var dynamicParameters = new DynamicParameters();
-            dynamicParameters.Add("@price", dbType: DbType.Decimal,
-                value: productSummary.Price, direction: ParameterDirection.Input, precision: 10, scale: 2);
-            using (var connection = _db)
+            using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
             {
-                IEnumerable<dynamic> results = connection.Query(query, dynamicParameters);
+                db.Execute(@"UPDATE [dbo].[Products] SET Name = @name, Price = @price WHERE Id = @id", new
+                {
+                    id = productSummary.Id,
+                    name = productSummary.Name,
+                    price = productSummary.Price
+                });
             }
         }
 
         public void DeleteProduct(Guid id)
         {
-            _db.Execute(@"DELETE FROM [PRODUCTS] WHERE id = @id",
+            using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                db.Execute(@"DELETE FROM [PRODUCTS] WHERE id = @id",
                 new { Id = id });
+            }
         }
     }
 }
